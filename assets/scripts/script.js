ff72d741b9352d00b36df36b89b477225031f10f
LegendaryFire <Tristan.Balon@Outlook.com>
2024-02-22 14:52:55 -0800

    Adjusted max width, refactored button logic, added transition effects.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,13 @@
 // Handle buttons for Welcome paragraph.
-const welcomeButton = document.getElementById("welcomeMore");
-welcomeButton.addEventListener("click", function () {
-  const welcome = document.getElementById("welcome");
-  if (welcome.classList.toggle("collapse")) {
-    welcomeButton.innerText = "See More";
-  } else {
-    welcomeButton.innerText = "See Less";
-  }
-});
-
-// Handle buttons for Welcome paragraph.
-const aboutButton = document.getElementById("aboutMore");
-aboutButton.addEventListener("click", function () {
-  const about = document.getElementById("about");
-  if (about.classList.toggle("collapse")) {
-    aboutButton.innerText = "See More";
-  } else {
-    aboutButton.innerText = "See Less";
-  }
+const buttonElems = document.querySelectorAll("button.collapsible");
+buttonElems.forEach((buttonElem) =>  {
+  buttonElem.addEventListener("click", function() {
+    let id = buttonElem.getAttribute("name");
+    const elem = document.getElementById(id);
+    if (elem.classList.toggle("show")) {
+      buttonElem.innerText = "See Less";
+    } else {
+      buttonElem.innerText = "See More";
+    }
+  });
 });
