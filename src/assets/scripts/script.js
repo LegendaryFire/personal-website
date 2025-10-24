238426a74819e5de51d3e7d21089de11e4035056
LegendaryFire <tristan.balon@outlook.com>
2025-10-24 15:57:21 -0700

    Large work in progress

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,13 +1,15 @@
 // Handle buttons for paragraphs.
-const actionButtons = document.querySelectorAll("button[data-target]");
-actionButtons.forEach((btnElement) => {
-  btnElement.addEventListener("click", function () {
-    let hideElement = btnElement.getAttribute("data-target");
-    hideElement = document.getElementById(hideElement);
-    if (hideElement.classList.toggle("show")) {
-      btnElement.innerText = "See Less";
-    } else {
-      btnElement.innerText = "See More";
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  const actionButtons = document.querySelectorAll("button[data-target]");
+  actionButtons.forEach((btnElement) => {
+    btnElement.addEventListener("click", function () {
+      let hideElement = btnElement.getAttribute("data-target");
+      hideElement = document.querySelector(`#${hideElement}`);
+      if (hideElement.classList.toggle("show")) {
+        btnElement.innerText = "See Less";
+      } else {
+        btnElement.innerText = "See More";
+      }
+    });
   });
 });
